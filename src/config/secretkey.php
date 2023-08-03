2e98c8ce8b6977a093a334acacd51a154cb561ba
kaushaltest <kaushaltest4346@gmail.com>
2023-08-04 04:16:57 +0530

    add secretkey in config

--- src/config/secretkey.php
+++ src/config/secretkey.php
@@ -1,5 +1,8 @@
 <?php
 
 return [
-    'key'=>'kaushal4346'
+    'google' => [
+        'client_id' => env('GOOGLE_CLIENT_ID', 'your_default_client_id'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET', 'your_default_client_secret'),
+    ],
 ];

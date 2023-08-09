a4e1e2dbbb31b74de3297586e9fcfd3a4edda751
kaushaltest <141280733+kaushaltest@users.noreply.github.com>
2023-08-09 11:45:59 +0530

    Update Sociallogincontroller.php

--- src/Http/Controllers/Sociallogincontroller.php
+++ src/Http/Controllers/Sociallogincontroller.php
@@ -12,4 +12,15 @@ class Sociallogincontroller extends Controller
         // echo config('secretkey.key');
         return Socialite::driver('google')->redirect();
     }
+      public function handleGoogleCallback()
+    {
+        $user = Socialite::driver('google')->user();
+        print_r($user);
+        
+        // Your login logic here, you can check if the user exists in the database and log them in.
+        // Example:
+        // $user->getId(); // Google ID
+        // $user->getName(); // User full name
+        // $user->getEmail(); // User email address
+    }
 }
